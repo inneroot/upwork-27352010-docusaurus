2336e4e25180b30c00e213489f0efd023fff8dc0
innercoding <innercoding@gmail.com>
2021-06-15 16:56:19 +0300

    add 8 bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import React from "react";
-// import clsx from "clsx";
-// import Layout from "@theme/Layout";
 import Navbar from "../components/home/Navbar";
-
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
 import SectionOne from "../components/home/SectionOne";
 import SectionTwo from "../components/home/SectionTwo";
 import Footer from "../components/home/Footer";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <main className="main__box nalozhenie">
       <Navbar />
