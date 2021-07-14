15a2ea44c11f65e4bd0bf83a6f2f625f53e35171
inneroot <innercoding@gmail.com>
2021-07-15 00:32:55 +0300

    add workflow

--- src/components/docs/building/Workflow.jsx
+++ src/components/docs/building/Workflow.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Workflow.module.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import { CARDS, BUTTONS } from "./cards";
+
+function Card({ cardNumber }) {
+  const paragraphs = CARDS[cardNumber].paragraphs.map((line) => {
+    const imgsrc = useBaseUrl(`/img/workflow/${line.img}.svg`);
+    return (
+      <div className={styles.line}>
+        <img src={imgsrc} />
+        <p>{line.text}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className={styles.card}>
+      <h3>
+        {CARDS[cardNumber].heading}
+        {CARDS[cardNumber].hint && (
+          <span className={styles.hint}>{CARDS[cardNumber].hint}</span>
+        )}
+      </h3>
+      {paragraphs}
+    </div>
+  );
+}
 
 export default function Workflow() {
-  // const code = useBaseUrl("/img/code.svg");
+  const [cardNumber, setCardNumber] = useState(0); // const code = useBaseUrl("/img/code.svg");
+  const buttons = BUTTONS.map((label, index) => (
+    <a
+      className={cardNumber === index ? styles.active__button : null}
+      onClick={() => setCardNumber(index)}
+    >
+      <span>{"0" + (index + 1)}</span>
+      <label>{label}</label>
+    </a>
+  ));
 
   return (
     <section className={styles.container}>
-      <h3>Определение задачи и поиск решения</h3>
+      <Card cardNumber={cardNumber} />
+      <div className={styles.control__panel}>
+        <div className={styles.dashed}></div>
+        {buttons}
+        </div>
     </section>
   );
 }
