20f979e0f0a2a772505cc56b2bce3dded6dc1765
innercoding <innercoding@gmail.com>
2021-08-11 19:15:52 +0300

    mainpage mobile navigation

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "@docusaurus/Link";
 import styles from "./Navbar.module.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+const MobileLinks = ({ navLinks, isVisible }) => {
+  if (isVisible)
+    return (
+      <div className={styles.mobile_nav_links_box}>
+        <div className={styles.mobile_navigation_links}>
+          {navLinks.map((link) => (
+            <Link
+              to={link.docId ? `docs/${link.docId}` : link.to}
+              key={link.label}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
+        <Link to="#" className="nav_btn">
+          Задать вопрос
+        </Link>
+      </div>
+    );
+
+  return null;
+};
 
 export default function Navbar() {
   const { siteConfig } = useDocusaurusContext();
@@ -10,6 +33,11 @@ export default function Navbar() {
   const logoSrc = useBaseUrl("/img/logo.svg");
   const mmenux = useBaseUrl("/img/mmenux.svg");
   const mmenu = useBaseUrl("/img/mmenu.svg");
+
+  const [isVisible, setVisible] = useState(false);
+  const toggle = () => {
+    setVisible(!isVisible);
+  };
   return (
     <>
       <nav className={styles.navigation}>
@@ -31,9 +59,10 @@ export default function Navbar() {
         <div className={styles.logo}>
           <img src={logoSrc} />
         </div>
-        <button className={styles.hamburger_button}>
-          <img src={mmenu} />
+        <button className={styles.hamburger_button} onClick={toggle}>
+          <img src={isVisible ? mmenux : mmenu} />
         </button>
+        <MobileLinks navLinks={navLinks} isVisible={isVisible} />
       </nav>
     </>
   );
