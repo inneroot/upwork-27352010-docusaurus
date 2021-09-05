e3f4f87dbd4e0e9cf9e9006e60a5ec03b39f73f5
inneroot <innercoding@gmail.com>
2021-09-05 19:06:08 +0300

    mobile doc cicd - Opportunities

--- src/components/docs/building/Hero.jsx
+++ src/components/docs/building/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Hero() {
+  const cicdmobile = useBaseUrl("/img/cicdmobile.svg");
   return (
     <section className={styles.section}>
       <h1>Построение процессов CI/CD</h1>
@@ -13,6 +15,7 @@ export default function Hero() {
           эксплуатацию <span>{"}"}</span>
         </li>
       </ul>
+      <img className={styles.cicdmobile} src={cicdmobile} />
     </section>
   );
 }
