93e56a4cd4298ee84732606d22485a1f6748c89f
inneroot <innercoding@gmail.com>
2021-09-06 01:43:29 +0300

    contacts mobile

--- src/components/docs/building/Contacts.jsx
+++ src/components/docs/building/Contacts.jsx
@@ -5,25 +5,49 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 export default function Contacts() {
   const LogoSrc = useBaseUrl("/img/logo.svg");
   return (
-    <div id={styles.business_card}>
-      <div id={styles.business_card_slogan}>
-        <img id={styles.business_card_logo} src={LogoSrc} />
-        <p>Профессиональные практики DevOps</p>
+    <>
+      <div id={styles.business_card}>
+        <div id={styles.business_card_slogan}>
+          <img id={styles.business_card_logo} src={LogoSrc} />
+          <p>Профессиональные практики DevOps</p>
+        </div>
+        <div id={styles.business_card_contacts}>
+          <a className={styles.card_email} href="mailto:it-company@info.ru">
+            it-company@info.ru
+          </a>
+          <a className={styles.card_telegram} href="#">
+            Telegram
+          </a>
+          <a className={styles.card_phone} href="phone:+79992223344">
+            +7 (999) 222-33-44
+          </a>
+          <a className={styles.card_whatsapp} href="#">
+            WhatsApp
+          </a>
+        </div>
       </div>
-      <div id={styles.business_card_contacts}>
-        <a className={styles.card_email} href="mailto:it-company@info.ru">
-          it-company@info.ru
-        </a>
-        <a className={styles.card_telegram} href="#">
-          Telegram
-        </a>
-        <a className={styles.card_phone} href="phone:+79992223344">
-          +7 (999) 222-33-44
-        </a>
-        <a className={styles.card_whatsapp} href="#">
-          WhatsApp
-        </a>
+
+      <div id={styles.business_card__mobile}>
+        <img id={styles.business_card_logo__mobile} src={LogoSrc} />
+        <p>
+          Профессиональные
+          <br /> практики DevOps
+        </p>
+        <div id={styles.business_card_contacts__mobile}>
+          <a className={styles.card_email} href="mailto:it-company@info.ru">
+            it-company@info.ru
+          </a>
+          <a className={styles.card_phone} href="phone:+79992223344">
+            +7 (999) 222-33-44
+          </a>
+          <a className={styles.card_telegram} href="#">
+            Telegram
+          </a>
+          <a className={styles.card_whatsapp} href="#">
+            WhatsApp
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
