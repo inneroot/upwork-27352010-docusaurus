11ebee6ff1249366d69101a5e12c013e72739ce4
inneroot <innercoding@gmail.com>
2021-10-18 10:56:42 +0300

    Contacts from config

--- src/components/common/Contacts.jsx
+++ src/components/common/Contacts.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./Contacts.module.css";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 export default function Contacts() {
   const LogoSrc = useBaseUrl("/img/logo.svg");
+  const { siteConfig } = useDocusaurusContext();
+  const contacts = siteConfig.customFields.contacts;
   return (
     <>
       <div id={styles.business_card}>
@@ -12,16 +15,16 @@ export default function Contacts() {
           <p>Профессиональные практики DevOps</p>
         </div>
         <div id={styles.business_card_contacts}>
-          <a className={styles.card_email} href="mailto:it-company@info.ru">
-            it-company@info.ru
+          <a className={styles.card_email} href={`mailto:${contacts.email}`}>
+            {contacts.email}
           </a>
-          <a className={styles.card_telegram} href="#">
+          <a className={styles.card_telegram} href={contacts.telegram}>
             Telegram
           </a>
-          <a className={styles.card_phone} href="phone:+79992223344">
-            +7 (999) 222-33-44
+          <a className={styles.card_phone} href={contacts.phone_href}>
+            {contacts.phone_label}
           </a>
-          <a className={styles.card_whatsapp} href="#">
+          <a className={styles.card_whatsapp} href={contacts.whatsapp}>
             WhatsApp
           </a>
         </div>
